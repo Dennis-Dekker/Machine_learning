1e6776cccfde13fe8b13ec83f38355786b6a0935
DavideBrex <davidebx96@gmail.com>
2019-03-01 12:28:42 +0100

    import data

--- Classification.py
+++ Classification.py
@@ -1,1 +1,13 @@
+import numpy as np
+import pandas as pd
+import glob
+import sklearn
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectKBest
+from sklearn.decomposition import PCA
+import matplotlib
+import matplotlib.pyplot as plt
 
+def main():
+        
