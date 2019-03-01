660fcc966535806b48770794484412c481125fe7
Dennis Dekker <48018095+Dennis-Dekker@users.noreply.github.com>
2019-03-01 13:20:32 +0100

    update script

--- create_dataset.py
+++ create_dataset.py
@@ -1,3 +1,5 @@
+
+
 import synapseclient
  
 syn = synapseclient.Synapse()
@@ -6,7 +8,7 @@ syn.login('Machine_learning_project_70','Group_70')
 # Obtain a pointer and download the data
 syn2320114 = syn.get(entity='syn2320114')
  
-head(syn2320114)
+print(type(syn2320114))
  
 print(syn2320114.name)
 print(syn2320114.path)
