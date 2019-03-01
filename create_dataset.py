07d2bded60d391d3baacbf69ed02d81b370c0ed2
Dennis Dekker <48018095+Dennis-Dekker@users.noreply.github.com>
2019-03-01 13:23:11 +0100

    update script

--- create_dataset.py
+++ create_dataset.py
@@ -1,5 +1,4 @@
-
-
+import numpy as np
 import synapseclient
  
 syn = synapseclient.Synapse()
@@ -8,7 +7,20 @@ syn.login('Machine_learning_project_70','Group_70')
 # Obtain a pointer and download the data
 syn2320114 = syn.get(entity='syn2320114')
  
+# inspect properties
 print(type(syn2320114))
  
 print(syn2320114.name)
 print(syn2320114.path)
+
+## load the data matrix into a dictionary with an entry for each column
+with open(syn2320114.path, 'r') as f:
+    labels = f.readline().strip().split('\t')
+    data = {label: [] for label in labels}
+    for line in f:
+        values = [float(x) for x in line.strip().split('\t')]
+        for i in range(len(labels)):
+            data[labels[i]].append(values[i])
+
+## load the data matrix into a numpy array
+np.loadtxt(fname=syn2320114.path, skiprows=1)
