33c9f2bc9e3a0449e9eb31cbb03d62cfdaef8e1f
DavideBrex <davidebx96@gmail.com>
2019-03-01 12:50:58 +0100

    ss

--- Classification.py
+++ Classification.py
@@ -8,6 +8,10 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.decomposition import PCA
 import matplotlib
 import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.metrics import confusion_matrix 
+from sklearn.model_selection import train_test_split 
+
 
 def main():
     Data = pd.read_csv("data/PCA_transformed_data.csv", header=None)
@@ -16,6 +20,10 @@ def main():
     print(Labels.head())
 
     X_train, X_test, y_train, y_test = train_test_split(Data, Labels, random_state = 0) 
-    
+    dtree_model = DecisionTreeClassifier(max_depth = 2).fit(X_train, y_train) 
+    dtree_predictions = dtree_model.predict(X_test) 
+  
+    # creating a confusion matrix 
+    cm = confusion_matrix(y_test, dtree_predictions)
 
 main()
