6b1511a30521b4c2695f6f5bddaf44bdd0a5b5fd
DavideBrex <davidebx96@gmail.com>
2019-03-01 13:14:13 +0100

    tree_classifier

--- Classification.py
+++ Classification.py
@@ -18,12 +18,18 @@ def main():
     Labels=pd.read_csv("data/labels.csv")
     print(Data.head())
     print(Labels.head())
-
-    X_train, X_test, y_train, y_test = train_test_split(Data, Labels, random_state = 0) 
-    dtree_model = DecisionTreeClassifier(max_depth = 2).fit(X_train, y_train) 
+    #split dataset in training and testing
+    X_train, X_test, y_train, y_test = train_test_split(Data, Labels[["Class"]], random_state = 0) 
+    
+    #decision tree classifier
+    dtree_model = DecisionTreeClassifier().fit(X_train, y_train) 
     dtree_predictions = dtree_model.predict(X_test) 
-  
     # creating a confusion matrix 
     cm = confusion_matrix(y_test, dtree_predictions)
+    print("Decision tree")
+    print(cm)
+
+
+
 
 main()
