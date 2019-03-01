2caff2d417236ea3ea194519277c6221713c8dda
Dennis Dekker <48018095+Dennis-Dekker@users.noreply.github.com>
2019-03-01 13:16:00 +0100

    update script

--- create_dataset.py
+++ create_dataset.py
@@ -6,6 +6,7 @@ syn.login('Machine_learning_project_70','Group_70')
 # Obtain a pointer and download the data
 syn2320114 = syn.get(entity='syn2320114')
  
+head(syn2320114)
+ 
 print(syn2320114.name)
-print(syn2320114.description)
 print(syn2320114.path)
