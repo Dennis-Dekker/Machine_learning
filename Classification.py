d796b5f5418a42545497af884983c26e16a04d69
DavideBrex <davidebx96@gmail.com>
2019-03-01 13:24:12 +0100

    add classifier

--- Classification.py
+++ Classification.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier 
 from sklearn.metrics import confusion_matrix 
 from sklearn.model_selection import train_test_split 
+from sklearn.svm import SVC 
+
 
 
 def main():
@@ -29,7 +31,18 @@ def main():
     print("Decision tree")
     print(cm)
 
-
+    # training a linear SVM classifier 
+    svm_model_linear = SVC(kernel = 'polinomial', C = 1).fit(X_train, y_train) 
+    svm_predictions = svm_model_linear.predict(X_test) 
+    
+    # model accuracy for X_test   
+    accuracy = svm_model_linear.score(X_test, y_test) 
+    
+    # creating a confusion matrix 
+    cm = confusion_matrix(y_test, svm_predictions) 
+    print("SVM")
+    print(cm)
+    print(accuracy)
 
 
 main()
