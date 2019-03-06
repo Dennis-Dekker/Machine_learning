6bceedca8a7e99222ac8bf48fe549f6fa2ad1904
DavideBrex <davidebx96@gmail.com>
2019-03-06 11:40:42 +0100

    added comments

--- Classification.py
+++ Classification.py
@@ -201,7 +201,7 @@ def main():
     cm_knn, accuracy_knn, knn=k_nearest_neighbors(X_train, X_test, y_train, y_test)
     plot_accuracy("K-NN", cm_knn, accuracy_knn)
 
-    #roc plot --> IS NOT WORKING (IDK why)
+    #roc plot --> takes a lot for svm
     roc_plot(X_train, y_train, X_test, y_test, svm_model, tree, knn)
 
 
